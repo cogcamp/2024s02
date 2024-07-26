a08b1641b7d053c0cca92db77049154d3e46917d
cogcamp <codeofgenius.camp@gmail.com>
2024-07-26 12:22:10 +0000

    Shion_4

--- Shion_4/main.js
+++ Shion_4/main.js
@@ -40,6 +40,18 @@ mainScene.create = function() {
         font: '20px Open Sans',
         fill: '#ff0000'
     });
+    //応援テキスト
+    if(this.blocks.countActive() > 162) {
+        this.CHEER = '頑張って！';
+    } else if(this.blocks.countActive() > 81) {
+        this.CHEER = '大丈夫！';
+    } else {
+        this.CHEER = 'あと少し〜';
+    }
+    this.cheerText =this.add.text(150, 460, '' + this.CHEER, {
+        font: '20px Open Sans',
+        fill: '#ff0000'
+    });
 };
 
 mainScene.update = function() {
