52c5f6388906cf483087c721ddfc2dc15e04cfee
cogcamp <codeofgenius.camp@gmail.com>
2024-07-26 12:52:24 +0000

    Shion_4

--- Shion_4/main.js
+++ Shion_4/main.js
@@ -73,7 +73,7 @@ mainScene.config = function() {
     this.cameras.main.setBackgroundColor('#cccccc');
     
     // パドルの移動速度
-    this.paddleSpeed = 111;
+    this.paddleSpeed = 11;
     
     // ボール発射の加速度
     this.ballSpeedX = 0;
